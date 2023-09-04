e59740f6377719e36d192d5b06a8c65a6a532ceb
Gihun Ham <ham@e-mirim.hs.kr>
2023-09-04 13:47:17 +0900

    인덱서 활용 예제

--- IndexerPrac31/Program.cs
+++ IndexerPrac31/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace IndexerPrac31
 {
+    class SquareCalculator
+    {
+        public int this[int i]
+        {
+            get
+            {
+                return i * i;
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            SquareCalculator square = new SquareCalculator();
+            Console.WriteLine(square[10]);
         }
     }
 }
